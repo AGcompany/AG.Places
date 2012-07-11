a324fc3f2b35ffed7e143ea24e746f63c88e254f
Bogdan Girman <bogdan.girman@zojax.com>
2012-07-11 20:22:19 +0300

    Initialize install steps

---
+++ runonce.sh
@@ -0,0 +1,4 @@
+
+apt-get update
+apt-get install -y -q python-elementtree python-libxml2 python-lxml python-imaging python-ldap python-simplejson python-crypto python-setuptools xpdf wv build-essential python-dev
+
