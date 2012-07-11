5e13ce5b6b0767eb825de4e6e3b460a66a845ea3
Bogdan Girman <bogdan.girman@zojax.com>
2012-07-11 21:41:40 +0300

    re #8 FIXED:Couldn't install: lxml 2.3.4

--- runonce.sh
+++ runonce.sh
@@ -13,5 +13,19 @@ apt-get install -y -q \
                       build-essential\
                       python-dev
 
+
 # Install dependencies
 easy_install zc.buildout==1.4.4
+
+# FIXED 
+# An error occured when trying to install lxml 2.3.4. Look above this message for any errors that were output by easy_install.
+# While:
+#   Installing instance.
+#   Getting distribution for 'lxml==2.3.4'.
+# Error: Couldn't install: lxml 2.3.4
+# *************** PICKED VERSIONS ****************
+
+apt-get install -y -q libxml2
+apt-get install -y -q python-libxslt1
+apt-get install -y -q libxslt-dev
+easy_install lxml
