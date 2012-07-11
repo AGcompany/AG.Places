677122a9b60d4594e6afb260c07a11ec33a3c9ac
Bogdan Girman <bogdan.girman@zojax.com>
2012-07-11 20:27:11 +0300

    Updater runonce.sh

--- runonce.sh
+++ runonce.sh
@@ -1,4 +1,15 @@
 
 apt-get update
-apt-get install -y -q python-elementtree python-libxml2 python-lxml python-imaging python-ldap python-simplejson python-crypto python-setuptools xpdf wv build-essential python-dev
+apt-get install -y -q \
+python-libxml2\
+python-lxml\
+python-imaging\
+python-ldap\
+python-simplejson\
+python-crypto\
+python-setuptools\
+xpdf\
+wv\
+build-essential\
+python-dev
 
