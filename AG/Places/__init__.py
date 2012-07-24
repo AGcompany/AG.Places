4fc18348354f6c7c805821818b993be13ce0f448
Bogdan Girman <bogdan.girman@gmail.com>
2012-07-24 08:26:29 +0300

    Added AG.Places package

---
+++ AG/Places/__init__.py
@@ -0,0 +1,9 @@
+# This package may contain traces of nuts
+# -*- coding: utf-8 -*-
+
+# from zope.i18nmessageid import MessageFactory as BaseMessageFactory
+# MessageFactory = BaseMessageFactory('AG.Places')
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
+
---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='AG.Places',
+      version=version,
+      description="Plone Places site policy",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        ],
+      keywords='',
+      author='Bogdan Girman',
+      author_email='bogdan.girman@gmail.com',
+      url='https://github.com/AGcompany/AG.Places',
+      license='gpl',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['AG'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
+
